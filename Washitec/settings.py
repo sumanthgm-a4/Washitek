559fffc768bdebda7da7a3f7102910626ee5d196
sum <214g1a32a4@srit.ac.in>
2024-09-05 23:42:21 +0530

    Some Commit

--- Washitec/settings.py
+++ Washitec/settings.py
@@ -129,3 +129,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+# Email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "sumanthgm12345@gmail.com"
+EMAIL_HOST_PASSWORD = "iljm oyfi mqnn bpju"
--- newapp/views.py
+++ newapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from newapp.models import *
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from django.conf import settings
+from django.core.mail import send_mail
+from django.contrib import messages
+from django.db.utils import IntegrityError
 
 # Create your views here.
 def render_home(request):
@@ -36,11 +40,57 @@ def render_elite_plus(request):
 
 def render_contact(request):
     
+    if request.method == "POST":
+        firstname  = request.POST.get("first-name")
+        lastname = request.POST.get("last-name")
+        mobile = request.POST.get("mobile")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+        send_mail(
+            from_email=email, 
+            subject=f"Mail from {firstname} {lastname} - {email}",
+            message=message,
+            recipient_list=['sumanthgm12345@gmail.com'],
+            fail_silently=False
+        )
+        messages.success(request, "Mail sent successfully")
+        
     return render(request, "contact.html")
 
 
 def render_login(request):
+    if request.user.is_authenticated:
+        messages.error(request, 'YOU LOGGED IN...')
+        return redirect('home')
     
+    if request.method == 'POST':
+        identifier = request.POST['identifier']
+        password = request.POST['passw']
+        print(f"Identifier: {identifier}")
+        print(f"Password: {password}")
+        if '@' in identifier:
+            try:
+                user = User.objects.get(email=identifier)
+                username = user.username
+            except User.DoesNotExist:
+                username = None
+        else:
+            try:
+                customer = Customer.objects.get(mobile=identifier)
+                username = customer.userobj.username
+            except Customer.DoesNotExist:
+                username = None
+
+        if username:
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home') 
+            else:
+                messages.error(request, 'Invalid credentials')
+        else:
+            messages.error(request, 'Invalid identifier')
+
     return render(request, "login.html")
 
 
@@ -51,8 +101,39 @@ def render_logout(request):
 
 
 def render_register(request):
+    if request.method == 'POST':
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        gender = request.POST['gender']
+        year = request.POST['year']
+        cname = request.POST['cname']
+        branch = request.POST['branch']
+        mobile = request.POST['mobile']
+        email = request.POST['email']
+        passw = request.POST['passw']
+        cpassw = request.POST['cpassw']
+
+        if passw == cpassw:
+            if User.objects.filter(username=fname).exists():
+                return render(request, "signup.html", {'error': 'Username already exists'})
+            try:
+                user = User.objects.create_user(username=fname, password=passw, email=email, first_name=fname, last_name=lname)
+                user.save()
+                obj = Customer(userobj=user, gender=gender, year=year, college=cname, branch=branch, mobile=mobile)
+                obj.save()
+                return redirect("login")  
+            except IntegrityError:
+                return render(request, "signup.html", {'error': 'An error occurred while creating the user'})
+        else:
+            return render(request, "signup.html", {'error': 'Passwords do not match'})
+    else:
+        return render(request, "signup.html")
+
+
+def render_free_pickup(request):
     
-    return render(request, "signup.html")
+    return render(request, "Free_pick_up.html")
+
 
 
 def render_free_pickup(request):
